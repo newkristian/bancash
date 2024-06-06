911572606f5719e3483dbfe68298124d6d0420fa
kristianconk <christiansoto6@gmail.com>
2024-06-06 11:41:42 -0600

    Initialize Firebase

---
+++ app/src/main/java/me/kristianconk/bancash/BancashApp.kt
@@ -0,0 +1,12 @@
+package me.kristianconk.bancash
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class BancashApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
